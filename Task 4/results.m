7b3f54e36a4f2c08ee85971cf54bc19b0f0ba143
Mariana Petropoulou <marianapetropoulou@gmail.com>
2023-02-05 19:49:26 +0200

    task 4 changes

--- Task 4/results.m
+++ Task 4/results.m
@@ -5,18 +5,16 @@ result=[];
 %non_zero=W(W~=0);
 
 rows_table=[];
-for m=1:4
-    n = randsample(500,1);
-    rows_table(end+1)=n;
-end
+
+rows_table = randsample(500,4);
+
 
 for r=1:length(rows_table)
-    most_similar=find(W(rows_table(r),:))
+    [x1,most_similar] = maxk(W(rows_table(r),:),4)
     figure;
     for i=1:length(most_similar)     
         [x,y]=imread(file{most_similar(i)});
         subplot(1,4,i),imshow(x,y);
-        hold on;
     end
 end
 
